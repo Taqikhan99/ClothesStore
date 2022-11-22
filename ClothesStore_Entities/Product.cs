6da0803260e5e61f47026cfef7982eaff4642f81
TaqiAhmed <taqiahmed339@gmail.com>
2022-11-22 16:35:50 +0500

    Added Entities Project with Product and Category Classes

---
+++ ClothesStore_Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothesStore_Entities
+{
+    public class Product:BaseEntity
+    {
+        public Category Category { get; set; }
+    }
+}
