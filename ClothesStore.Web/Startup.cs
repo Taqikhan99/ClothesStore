a2e8e2db81e6f49245096681bc7cec8ddb100e38
TaqiAhmed <taqiahmed339@gmail.com>
2022-11-22 15:05:53 +0500

    Add project files.

---
+++ ClothesStore.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClothesStore.Web.Startup))]
+namespace ClothesStore.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
