50c07cb5d46054ce7742cfe4e1c4e93dd31bbce9
TaqiAhmed <taqiahmed339@gmail.com>
2022-11-23 12:42:43 +0500

    added bootstrap 4.6, added category controller

---
+++ ClothesStore.Web/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using ClothesStore_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothesStore.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            return View();
+        }
+    }
+}
